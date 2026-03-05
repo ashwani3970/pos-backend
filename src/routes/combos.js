8249920d7c2840e7da2460d1e665f9bb44292dba
Ashwani <ashwani.hp@gmail.com>
2026-03-05 15:55:51 +0530

    combo15

--- src/routes/combos.js
+++ src/routes/combos.js
@@ -112,15 +112,14 @@ router.post("/orders/live/:orderId/combo", auth, async (req, res) => {
 
     const [parentResult] = await db.query(
       `INSERT INTO live_order_items
-      (live_order_id, combo_id, price, qty, is_combo_parent)
-      VALUES (?, ?, ?, ?, 1)`,
+      (live_order_id, combo_id, price, qty, is_combo_parent, kds_status)
+      VALUES (?, ?, ?, ?, 1, 'DONE')`,
       [
         orderId,
         combo_id,
         combo.combo_price,
         1
       ]
-
     );
 
     const parentId = parentResult.insertId;
